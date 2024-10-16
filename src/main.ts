dd88cae06014d49447ce42336c81d02eadd835cf
Mull San <MullSan@apples-MacBook-Pro.local>
2024-10-17 01:49:32 +0530

    new-theme-plus-animations

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { loadFonts } from './plugins/webfontloader'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'animate.css';
+import Typed from 'typed.js';
 import router from './router'
 import vuetify from './plugins/vuetify'
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,62 @@
 <template>
-  <WelcomeView />
-  <ProjectView />
-  <ExperienceView />
-  <ContactView />
+  <div id="welcome">
+    <WelcomeView @scroll-to-element="scrollToElement" />
+  </div>
+  <div id="projects">
+    <ProjectView />
+  </div>
+  <div id="experience">
+    <ExperienceView />
+  </div>
+  <div id="contact">
+    <ContactView />
+  </div>
+  <v-fab
+    v-show="showGotoTopBtn"
+    color="#fff"
+    icon="fas fa-up-long"
+    location="top end"
+    size="64"
+    app
+    appear
+    @click="scrollToElement('welcome', 0)"
+  ></v-fab>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
   import WelcomeView from "./WelcomeView.vue";
   import ProjectView from "./ProjectView.vue";
   import ExperienceView from "./ExperienceView.vue";
   import ContactView from "./ContactView.vue";
+
+  const showGotoTopBtn = ref(false);
+
+  const scrollToElement = (elementId, offset = 0) => {
+    if (elementId !== 'welcome') {
+      showGotoTopBtn.value = true;
+    } else {
+      showGotoTopBtn.value = false;
+    }
+
+    const element = document.getElementById(elementId);
+    if (!element) return;
+
+    const elementRect = element.getBoundingClientRect();
+    const viewportHeight = window.innerHeight;
+
+    const scrollPosition = elementRect.top - viewportHeight + elementRect.height + offset;
+
+    window.scrollTo({
+      top: scrollPosition,
+      behavior: 'smooth'
+    });
+  };
 </script>
+
+<style>
+  div {
+    padding: 0;
+    margin: 0;
+  }
+</style>
